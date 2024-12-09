77ba59685ff2266163176c03a569c337d1fc3b69
Adithya-304 <adithyaadulapuram123@gmail.com>
2024-12-09 15:12:08 +0530

    Update Factor.java

--- Factor.java
+++ Factor.java
@@ -1,13 +1,5 @@
 public class Factor {
     public static void main(String[] args) {
-        int number = 4;
-
-        System.out.println("Factors of " + number + ":");
-
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
-                System.out.println(i);
-            }
-        }
+        System.out.println("Heloo");
     }
 }
